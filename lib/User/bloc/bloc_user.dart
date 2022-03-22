584196ae571ca6da4b9d1ecbe2de10d8bf262e17
cristianmilo07 <cristianmilo07@gmail.com>
2022-03-22 15:47:23 -0500

    Corrección de despliegue de la aplicación

--- lib/User/bloc/bloc_user.dart
+++ lib/User/bloc/bloc_user.dart
@@ -1,4 +1,4 @@
-import 'package:generic_bloc_provider/generic_bloc_provider.dart/';
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
 
 class UserBloc implements Bloc {
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:avanzadoaprendizaje/aprendizaje_trips.dart';
 import 'package:flutter/material.dart';
 import 'aprendizaje_trips.dart';
-import'package:generic_bloc_provider/generic_bloc_provider.dart';
-import'package:avanzadoaprendizaje/User/bloc/bloc_user.dart';
 
 
 void main() {
@@ -17,11 +15,22 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-        child: MaterialApp(
-        title: 'Flutter Demo',
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
         home: AprendizajeTrips()//MyHomePage(title: 'Flutter Demo Home Page'),
-    ), bloc: UserBloc());
+    );
   }
 }
 
